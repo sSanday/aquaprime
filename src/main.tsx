cf84f535dde4e70e5e0a0cfab7721b7ca803bfcc
sandizakaria77 <sandizakaria77@gmail.com>
2025-12-11 21:04:06 +0000

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { UserProvider } from "./contexts/UserContext.tsx";
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "YOUR_CLIENT_ID";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "900204021320-k615vikpqahen43qm5g5teo1f20ek0d0.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
     <GoogleOAuthProvider clientId={clientId}>
